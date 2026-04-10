214e5fce39db371bad356821bec0332f70131a0d
kastriotjonuzi <kastriot.jo@hotmail.com>
2026-04-11 01:10:54 +0200

    LN04 anpassung ausgabe

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course!');
+  res.send('Hello Kastriot! LN04 FS2026 :-)');
 });
 
 app.listen(PORT, HOST);
